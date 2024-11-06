a95ef6bd9081a27f8768ef482d7e3e234f34a73f
aditonorman <muhammad.raditya33@ui.ac.id>
2024-11-06 11:32:15 +0700

    tut7

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,27 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:mental_health_tracker/widget/left_drawer.dart';
+
+import 'moodentry_form.dart';
+import '../widget/mood_card.dart';
 
 class MyHomePage extends StatelessWidget {
-    MyHomePage({super.key});
-    final String npm = '2306256450'; // NPM
-    final String name = 'Muhammad Raditya Indrastata Norman'; // Name
-    final String className = 'PBP KKI'; // Class
-    final List<ItemHomepage> items = [
+  final String npm = '2306256450'; // NPM
+  final String name = 'Muhammad Raditya Indrastata Norman'; // Name
+  final String className = 'KKI'; // Class
+  final List<ItemHomepage> items = [
     ItemHomepage("View Mood", Icons.mood),
     ItemHomepage("Add Mood", Icons.add),
     ItemHomepage("Logout", Icons.logout),
-     ];
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+  ];
+  MyHomePage({super.key});
 
   @override
-   @override
   Widget build(BuildContext context) {
     // Scaffold provides the basic structure of the page with the AppBar and body.
     return Scaffold(
@@ -37,7 +31,9 @@ class MyHomePage extends StatelessWidget {
         ),
         // The background color of the AppBar is obtained from the application theme color scheme.
         backgroundColor: Theme.of(context).colorScheme.primary,
+        iconTheme: const IconThemeData(color: Colors.white),
       ),
+      drawer: const LeftDrawer(),
       // Body of the page with paddings around it.
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -105,8 +101,8 @@ class MyHomePage extends StatelessWidget {
 class InfoCard extends StatelessWidget {
   // Card information that displays the title and content.
 
-  final String title;  // Card title.
-  final String content;  // Card content.
+  final String title; // Card title.
+  final String content; // Card content.
 
   const InfoCard({super.key, required this.title, required this.content});
 
@@ -117,7 +113,8 @@ class InfoCard extends StatelessWidget {
       elevation: 2.0,
       child: Container(
         // Set the size and spacing within the card.
-        width: MediaQuery.of(context).size.width / 3.5, // Adjust with the width of the device used.
+        width: MediaQuery.of(context).size.width /
+            3.5, // Adjust with the width of the device used.
         padding: const EdgeInsets.all(16.0),
         // Place the title and content vertically.
         child: Column(
@@ -133,67 +130,4 @@ class InfoCard extends StatelessWidget {
       ),
     );
   }
-}
-
-
-class ItemHomepage {
-    final String name;
-    final IconData icon;
-
-    ItemHomepage(this.name, this.icon);
-}
-
-
-class ItemCard extends StatelessWidget {
-  // Display the card with an icon and name.
-
-  final ItemHomepage item; 
-  
-  const ItemCard(this.item, {super.key}); 
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      // Specify the background color of the application theme.
-      color: Theme.of(context).colorScheme.secondary,
-      // Round the card border.
-      borderRadius: BorderRadius.circular(12),
-      
-      child: InkWell(
-        // Action when the card is pressed.
-        onTap: () {
-          // Display the SnackBar message when the card is pressed.
-          ScaffoldMessenger.of(context)
-            ..hideCurrentSnackBar()
-            ..showSnackBar(
-              SnackBar(content: Text("You have pressed the ${item.name} button!"))
-            );
-        },
-        // Container to store the Icon and Text
-        child: Container(
-          padding: const EdgeInsets.all(8),
-          child: Center(
-            child: Column(
-              // Place the Icon and Text in the center of the card.
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  item.icon,
-                  color: Colors.white,
-                  size: 30.0,
-                ),
-                const Padding(padding: EdgeInsets.all(3)),
-                Text(
-                  item.name,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(color: Colors.white),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-  
 }
